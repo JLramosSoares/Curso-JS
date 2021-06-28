f458fb7e63d8bfa734100d8460c767715ab8513e
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-06-28 10:41:57 -0300

    Adcionando os exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log(`A velocidade do seu carro é: ${vel}Km/h.`)
+
+if (vel > 60){ //Condição Simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = "França"
+
+console.log(`Vivendo no ${país}.`)
+
+if (país == "Brasil"){ // Seleção Composta
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log("Você é Estrangeiro!")
+}
