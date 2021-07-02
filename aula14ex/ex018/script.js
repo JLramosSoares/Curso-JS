d65701c173b38e3fa7e708944fe3966c125a8d89
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-07-02 11:03:41 -0300

    Tabuada

--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -1,1 +1,18 @@
-window.alert("Olá")
+function calcular(){
+    var numbertxt = window.document.querySelector('input#numbertxt') 
+    var tabuada = window.document.querySelector('select#seltab') 
+    var produto;
+
+    if (numbertxt.value.length == 0){
+        alert("Por favor, digite um número.")
+    } else {
+        var number = Number(numbertxt.value)
+        tabuada.innerHTML = ''
+        for (var c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+            produto = number * c
+            item.text = `${number} X ${c} = ${produto}`
+            tabuada.appendChild(item)
+        }
+    }
+}
